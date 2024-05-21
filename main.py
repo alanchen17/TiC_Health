2dd9f044ebae8c1334a2e63ccc80aac0ebcfbb90
achen677 <130784763+alanchen17@users.noreply.github.com>
2024-05-21 19:14:12 -0400

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import ijson
+import csv
+import datetime
+print(datetime.datetime.now())
+
+json_file_path = '2024-05-01_anthem_index.json'
+# json_file_path = 'test.json'
+csv_file_path = 'output_file.csv'
+with open(json_file_path, 'r') as json_file, open(csv_file_path, 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    objects = ijson.items(json_file, 'reporting_structure.item.in_network_files.item')
+    for item in objects:
+        des_details = item['description'].lower()
+        if "ny" in des_details or " new york" in des_details:
+            # if "ppo" in des_details:
+            writer.writerow(item.values())
+    csv_file.flush()
+    csv_file.close()
+
+print(datetime.datetime.now())
